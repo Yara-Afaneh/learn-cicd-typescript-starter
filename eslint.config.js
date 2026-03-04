00f8030223d7719fbfd528219b9d73cf169412e8
Yara-Afaneh <yara.afaneh@gmail.com>
2026-03-04 22:46:21 +0200

    Fix: correct eslint ignore syntax for v9

--- eslint.config.js
+++ eslint.config.js
@@ -5,19 +5,18 @@ import pluginSecurity from "eslint-plugin-security";
 
 export default [
   {
-    ignores: ["dist/**"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: {
-      js,
-      security: pluginSecurity,
-    },
-    languageOptions: {
-      globals: globals.node,
-    },
+    ignores: ["dist/**", "**/dist/*"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {},
+  },
 ];
